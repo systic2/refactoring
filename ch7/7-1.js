8c3917b6a92912b204158cadbed709a73918f90f
systic <llsysticll@gmail.com>
2023-03-18 00:03:32 +0900

    230317 7-3 레코드 캡슐화하기까지

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,42 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  #data;
+  constructor(data) {
+    this.#data = data;
+    this.#name = data.name;
+    this.#country = data.country;
+  }
 
-organization.name = 'Dream Coding';
+  get name() {
+    return this.#name;
+  }
+
+  // set name(value) {
+  //   this.#name = value;
+  // }
+
+  get country() {
+    return this.#country;
+  }
+
+  // set country(value) {
+  //   this.#country = value;
+  // }
+
+  get rawData() {
+    return { ...this.#data }; // 얕은복사, cloneDeep
+  }
+
+  // set 있는 경우
+  // get rawData() {
+  //   return { name: this.#name, country: this.#country, }; // 얕은복사, cloneDeep
+  // }
+}
+const organization = new Organization({
+  name: 'Acme Gooseberries',
+  country: 'GB',
+});
+
 console.log(organization.name);
 console.log(organization.country);
