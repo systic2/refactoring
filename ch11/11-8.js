8398b11b5da6bc79beafb9c5ac43f181bd7342ed
systic <llsysticll@gmail.com>
2023-04-19 22:29:00 +0900

    230419 11-9 생성자를 팩터리 함수로 바꾸기까지

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -14,4 +14,20 @@ export class Employee {
   static get legalTypeCodes() {
     return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
   }
+
+  static createEngineer(name) {
+    return new Employee(name, 'E');
+  }
+
+  static createManager(name) {
+    return new Employee(name, 'M');
+  }
+
+  static createSalesman(name) {
+    return new Employee(name, 'S');
+  }
 }
+
+const ellie = Employee.createEngineer('Ellie');
+const dream = Employee.createManager('Dream');
+const coding = Employee.createSalesman('Coding');
